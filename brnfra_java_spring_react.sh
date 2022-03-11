e8b80946c31f1890abfe8da0a678379521660521
Bruno Franco <37557220+brnfra@users.noreply.github.com>
2022-03-11 04:42:04 -0300

    Update brnfra_java_spring_react.sh
    echo -e

--- brnfra_java_spring_react.sh
+++ brnfra_java_spring_react.sh
@@ -9,28 +9,28 @@ HEROKU='heroku.tar.gz'
 #Download packages
 cd "/tmp" || return
 
-echo "\n\n Downloading Visual Studeo Code..."
+echo -e "\n\n Downloading Visual Studeo Code..."
 if $(curl -# -o $VSCODE -O "https://az764295.vo.msecnd.net/stable/c722ca6c7eed3d7987c0d5c3df5c45f6b15e77d1/code-stable-x64-1646928009.tar.gz") 
 then
     tar -xvf $VSCODE 
     echo "extract accomplished"
 fi
 
-echo "\n\n Downloading Spring Boot..."
+echo -e "\n\n Downloading Spring Boot..."
 if $(curl -# -o $SPRING -O "https://download.springsource.com/release/STS4/4.13.1.RELEASE/dist/e4.22/spring-tool-suite-4-4.13.1.RELEASE-e4.22.0-linux.gtk.x86_64.tar.gz") 
 then
     tar -xvf $SPRING
     echo "extract accomplished"
 fi
 
-echo "\n\n Downloading PostMan..."
+echo -e "\n\n Downloading PostMan..."
 if $(curl -# -o $POSTMAN -O "https://dl.pstmn.io/download/latest/linux64")
 then
     tar -xvf $POSTMAN 
     echo "extract accomplished"
 fi
 
-echo "\n\n Downloading Heroku-CLI..."
+echo -e "\n\n Downloading Heroku-CLI..."
 if $(curl -# -o $HEROKU -O "https://cli-assets.heroku.com/heroku-linux-x64.tar.gz")
 then
     tar -xvf $HEROKU
