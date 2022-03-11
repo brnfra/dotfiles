1b6f7319440da8deef2a414ba8ebceb06a6e5466
brnfra <devlabbr@outlook.com>
2022-03-10 21:34:50 -0300

    refactoring enviroment script

--- brnfra_default_enviroment.sh
+++ brnfra_default_enviroment.sh
@@ -17,17 +17,10 @@ wait $PROCESS_ID
 
 # create ~/.local dirs 
 echo -e "Criando pastas locais..."
-mkdir -p "$HOME/.local/opt"
-mkdir -p "$HOME/.local/share"
-mkdir -p "$HOME/.fonts"
-mkdir -p "$HOME/.wallpapers"
+mkdir -p "$HOME/.local/opt" "$HOME/.local/share" "$HOME/.fonts" "$HOME/.wallpapers"
 
 # user dirs
-mkdir -p "$HOME/documents/projects/git"
-mkdir "$HOME/downloads"
-mkdir "$HOME/music"
-mkdir "$HOME/pics"
-mkdir "$HOME/videos"
+mkdir -p "$HOME/documents/projects/git" "$HOME/downloads" "$HOME/music" "$HOME/pics" "$HOME/videos"
 
 #setup git(script from https://github.com/lewagon/dotfiles/blob/master/git_setup.sh )
 git config --global user.email "$email"
@@ -46,41 +39,30 @@ wait $PROCESS_ID
 eval "$(ssh-agent -s)"
 ssh-add "$HOME/.ssh/id_ed25519"
 
-#copy config files
 FILES='.vimrc .bashrc .bash_aliases .bash_profile .bash_functions '
 FILES+='.bash_exports .bash_prompt .bash_input .wgetrc .curlrc ' 
-FILES+='.tmux.conf .i3blocks.conf .i3status.conf '
-FILES+='.config/ '
-
-for F in $FILES; do
-    cp -r "$F" "/home/$USER"/ 
-done
+FILES+='.tmux.conf .i3blocks.conf .i3status.conf .config/ '
 
 # copy .config files
 echo -e "\nCopiando arquivos de configuração..."
+for F in $FILES; do
+    cp -r ./"$F" "/home/$USER"/ 
+done
+
+#Fonts
 cd "$HOME/.fonts" || return
 
-wget -O nerdFont.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip"
+wget -O nerdFont.zip \
+    "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip" & \
+    wget -O codeNewRoman.zip \
+    "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CodeNewRoman.zip" & \
+    wget -O Monofur.zip \
+    "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monofur.zip"
+
 PROCESS_ID=$!
 wait $PROCESS_ID
 
-unzip nerdFont.zip
-PROCESS_ID=$!
-wait $PROCESS_ID
-
-wget -O codeNewRoman.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CodeNewRoman.zip"
-PROCESS_ID=$!
-wait $PROCESS_ID
-
-unzip codeNewRoman.zip
-PROCESS_ID=$!
-wait $PROCESS_ID
-
-wget -O Monofur.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monofur.zip"
-PROCESS_ID=$!
-wait $PROCESS_ID
-
-unzip Monofur.zip
+unzip nerdFont.zip & unzip codeNewRoman.zip & unzip Monofur.zip
 PROCESS_ID=$!
 wait $PROCESS_ID
 
