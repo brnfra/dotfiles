c07b353a9b8708250dafdee06f8ececd181fb12b
Bruno Franco <37557220+brnfra@users.noreply.github.com>
2022-03-15 16:26:54 -0300

    undo move to folder action in install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -9,14 +9,13 @@ function config {
     git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@;
 }
 
-mkdir -p .config-backup
 config checkout
 
 if [ $? = 0 ]; then
     echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} mv -nuv {} .config-backup/{}.backup
+    config checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} mv {} {}.backup
 fi
 
 config checkout
