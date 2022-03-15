49c031e59e15c79f8f2d6464e384aabed692e16a
brnfra <devlabbr@gmail.com>
2022-03-15 15:38:34 -0300

    add script to update local dotfiles

---
+++ bin/up-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function config {
+    git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@;
+}
+
+mkdir -p .config-backup
+config pull
+
+
+if [ $? = 0 ]; then
+        echo "Checked out config.";
+else
+  echo "Backing up pre-existing dot files.";
+    config pull 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}.backup
+fi
+
+config pull
