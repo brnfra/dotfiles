5a12d4de82b05be324ff9e1a97e6845070d58bd4
brnfra <devlabbr@outlook.com>
2022-03-18 18:35:31 -0300

    update scritp

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -8,10 +8,16 @@ function config {
 
 echo "Status"
 config status
-config remote -v
-echo "Fetching..."
-config fetch origin master
-echo "Merging..."
-config merge --ff origin master
-echo "Status"
+
+mkdir -p "$BKPDIR"
+config pull --ff-only
+
+if [ $? = 0 ]; then
+        echo "Checked out config.";
+else
+  echo "Backing up pre-existing dot files.";
+  config pull --ff-only 2>&1 | egrep "^\s+\." | awk {'print $1'} | xargs -I{} mv "$HOME"/"{}" "$BKPDIR"
+fi
+
+config pull --ff-only
 config status
