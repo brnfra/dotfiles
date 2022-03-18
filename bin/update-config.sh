f4047f542d655e37ab8ec8feb72a8cb01ca355e3
brnfra <devlabbr@outlook.com>
2022-03-18 17:02:17 -0300

    add git stash to manage updates

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -6,19 +6,5 @@ function config {
     git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@;
 }
 
-# mkdir -p "$BKPDIR"
-
 config pull --autostash
 config stash drop
-
-
-
-
-# if [ $? = 0 ]; then
-#         echo "Checked out config.";
-# else
-#   echo "Backing up pre-existing dot files.";
-#   config pull 2>&1 | egrep "^\s+\." | awk {'print $1'} | xargs -I{} mv "$HOME"/"{}" "$BKPDIR"
-# fi
-
-# config pull  
