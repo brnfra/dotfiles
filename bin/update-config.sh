7f3979843e32f082cc03377cb2dddafc01368bca
brnfra <devlabbr@outlook.com>
2022-03-18 19:06:35 -0300

    update aliases(add upgonfig to pull configs). redo update-config

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -6,18 +6,15 @@ function config {
     git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@;
 }
 
-echo "Status"
-config status
 
 mkdir -p "$BKPDIR"
-config pull --ff-only
+config pull 
 
 if [ $? = 0 ]; then
         echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  config pull --ff-only 2>&1 | egrep "^\s+\." | awk {'print $1'} | xargs -I{} mv "$HOME"/"{}" "$BKPDIR"
+  config pull  2>&1 | egrep "^\s+\." | awk {'print $1'} | xargs -I{} mv "$HOME"/"{}" "$BKPDIR"
 fi
 
-config pull --ff-only
-config status
+config pull  
