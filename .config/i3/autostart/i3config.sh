1d9291bdfb1d9d425e493a72b79a4ca67fec53fa
brnfra <devlabbr@gmail.com>
2022-03-19 14:14:38 -0300

    teste de config para notebook

--- .config/i3/autostart/i3config.sh
+++ .config/i3/autostart/i3config.sh
@@ -7,8 +7,6 @@
 SCR="$(xrandr -q 2>/dev/null | grep -w connected | cut -d " " -f1 | cut -d "-" -f1 | sed s/1//)"
 #get primary screeen
 SCR1="$(xrandr -q 2>/dev/null | grep -w connected | cut -d " " -f1 | head -n 1)"
-#get secondary screen
-SCR2="$(xrandr -q 2>/dev/null | grep -w connected | cut -d " " -f1 | tail -n 1)"
 #My pathes
 PCONF="$HOME"
 PCONF+='/.config/i3/autostart'
@@ -18,6 +16,7 @@ PGIT+='/Documents/Projects/git/dotRepo/Privateconfig'
 
 # KEYBOARD config
 KEYB='setxkbmap -model pc104 -layout us_intl'
+KEYBABNT='setxkbmap -model abnt2 -layout br'
 
 #Hack to select the config related between virtual machines localization
 
@@ -34,14 +33,13 @@ if [ "$SCR" = "Virtual" ]; then
             fi
 else
 #config for primary machine
-    eval "$KEYB"
+    eval "$KEYBABNT"
 #set primary
     #set resolution full_hd
     eval "$PCONF/resolution.sh $SCR1 2>/dev/null"
-    eval "$PCONF/vertical.sh $SCR2 2>/dev/null"
     #set position
     eval "xrandr --output $SCR1 --primary"
     #turn left and put right of rotate the screen of your preference here SCR1 e SCR2
-    eval "xrandr --output $SCR1 --auto --rotate normal --output $SCR2 --auto --rotate right --right-of $SCR1 2>/dev/null"
+    eval "xrandr --output $SCR1 --auto --rotate normal 2>/dev/null"
     exit
 fi
