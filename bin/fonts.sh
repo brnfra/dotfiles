19df2b0aa211d3cda6e617f1d76f2a6b9ad900ee
brnfra <devlabbr@gmail.com>
2022-03-20 08:40:45 -0300

    Setting up my git configs

--- bin/fonts.sh
+++ bin/fonts.sh
@@ -2,36 +2,20 @@
 
 #Fonts
 cd "$HOME/.fonts" || return
-
 nerdfont="nerdfont.zip"
 codeNewRoman="codeNewRoman.zip"
 monofur="Monofur.zip"
 
 echo -e "\n\n Downloading Nerdfont..."
-if $(curl -# -k -o $nerdfont -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip") 
-then
-    unzip -xvf $nerdfont 
-    echo "extract accomplished"
-fi
-
-
+curl -C - -Lkso $nerdfont "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip" 
 echo -e "\n\n Downloading CoeNewRoman..."
-if $(curl -# -k -o $codeNewRoman -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CodeNewRoman.zip") 
-then
-    unzip -xvf $codeNewRoman 
-    echo "extract accomplished"
-fi
-
-
+curl -C - -Lkso $codeNewRoman "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CodeNewRoman.zip"
 echo -e "\n\n Downloading Monofur..."
-if $(curl -# -k -o $monofur -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monofur.zip") 
-then
-    unzip -xvf $monofur 
-    echo "extract accomplished"
-fi
+curl -C - -Lkso $monofur "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monofur.zip"
+
+unzip -uCoq $nerdfont $codeNewRoman $monofur
 
 fc-cache -vf "$HOME/.fonts/"
-
 echo -e "\nAll done!\n"
 
 exit 0
--- bin/update-config.sh
+++ bin/update-config.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+BKPDIR="$HOME"/.config-backup
+
 function config {
-    git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@;
+    git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@ 2>&1 | sed '/hint/d';
 }
 
-mkdir -p .config-backup
-config pull
+
+mkdir -p "$BKPDIR"
+config pull 
 
 if [ $? = 0 ]; then
         echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-    config pull 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}.backup
+  config pull  2>&1 | egrep "^\s+\." | awk {'print $1'} | xargs -I{} mv "$HOME"/"{}" "$BKPDIR"
 fi
 
-config pull
+config pull  
