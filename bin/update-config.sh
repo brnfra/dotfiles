8489836608b849f16135afeea8cd33de96387b6d
brnfra <devlabbr@outlook.com>
2022-03-30 12:56:47 -0300

    update bin

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -4,7 +4,7 @@ local=$(pwd)
 cd "$HOME" || return
 
 function config {
-    git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@ 2>&1 | sed '/hint/d';
+    git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@ ;
 }
 
 config pull 
@@ -12,7 +12,7 @@ config pull
 if [ $? = 0 ]; then
    echo "Checked out config.";
 else
-    config fetch
+    config fetch origin main
     config reset --hard HEAD
     config merge '@{u}'
 fi
