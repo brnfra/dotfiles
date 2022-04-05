07fec3ec4efbde44166fe4b84b2fa1e9b66eec35
brnfra <devlabbr@outlook.com>
2022-04-04 21:40:44 -0300

    update cfg script

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -5,11 +5,11 @@ $(cd "$HOME") || return
 function config {
     git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@ ; 
 }
-config pull origin main
+config pull origin notebook-cfg
 if [ $? = 0 ]; then
    echo "Checked out config.";
 else
-    config fetch origin main
+    config fetch origin notebook-cfg
     config reset --hard HEAD
     config merge '@{u}'
 fi
