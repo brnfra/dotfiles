20a2e60603a08cd25708ca5f697de6908fdee8cd
brnfra <devlabbr@outlook.com>
2022-04-05 03:47:31 -0300

    fix update script

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 local=$(pwd)
+BKPDIR="$HOME"/.config-backup/
+mkdir -p "$BKPDIR"
 $(cd "$HOME") || return
 function config {
     git --git-dir="$HOME"/.dotfiles/ --work-tree=$HOME $@ ; 
@@ -9,6 +11,9 @@ config pull origin notebook-cfg
 if [ $? = 0 ]; then
    echo "Checked out config.";
 else
+    config pull origin notebook-cfg
+    config pull  2>&1 | egrep "^\s+\." | awk {'print $1'} | xargs -I{} mv -f "$HOME"/{} "$BKPDIR"
+
     config fetch origin notebook-cfg
     config reset --hard HEAD
     config merge '@{u}'
