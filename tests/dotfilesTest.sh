3cbc23183ae9527d88e9673dfef7a889b328f931
brnfra <devlabbr@gmail.com>
2023-03-11 00:19:31 -0300

    fix: install script and refact for tests, typos, dotfiles_env strings

--- tests/dotfilesTest.sh
+++ tests/dotfilesTest.sh
@@ -5,22 +5,22 @@
 SHUNIT_TEST_PREFIX=' #Main --> '
 
 testDotfilesConfigs() {
-    printf "${cyan}${bold}[INFO]${reset} Check Git Configs Tests\n"
+    clnInfo "Check Git Configs Tests"
     "$HOME/tests/testGitConfig.sh"
     assertTrue ": 13 :${red}${bold}[FAIL]${reset} Error Git Configs Tests found\n" $?
-    printf "${cyan}${bold}[INFO]${reset} Check if Repo config files exists.\n"
+    clnInfo "Check if Repo config files exists."
     "$HOME/tests/testRepoConfigFiles.sh"
     assertTrue ": 15 :${red}${bold}[FAIL]${reset} Error Repo config files exists Tests found\n" $? 
-    printf "${cyan}${bold}[INFO]${reset} Check if Local config files exists.\n"
+    clnInfo "Check if Local config files exists."
     "$HOME/tests/testHomeConfigFiles.sh"
     assertTrue ": 17 :${red}${bold}[FAIL]${reset} Error Home config files exists Tests found.\n" $? 
-    printf "${cyan}${bold}[INFO]${reset} Check if Scripts in bin works[namestd].\n"
+    clnInfo "Check if Scripts in bin works[namestd]."
     "$HOME/tests/binNamestdScriptTest.sh"
     assertTrue ": 19 :${red}${bold}[FAIL]${reset} Error Scripts in bin works[namestd] Tests found\n" $? 
-    printf "${cyan}${bold}[INFO]${reset} Check if Scripts in bin works[fold by Date].\n"
+    clnInfo "Check if Scripts in bin works[fold by Date]."
     "$HOME/tests/binMakeFolderByDateTest.sh"
     assertTrue ": 21 :${red}${bold}[FAIL]${reset} Error Scripts Scripts in bin works[fold by Date]\n" $? 
-    printf "${cyan}${bold}[INFO]${reset} Check if Scripts in bin works[fold by Type].\n"
+    clnInfo "Check if Scripts in bin works[fold by Type]."
     "$HOME/tests/binMakeFolderByTypeTest.sh"
     assertTrue ": 21 :${red}${bold}[FAIL]${reset} Error Scripts Scripts in bin works[fold by Date]\n" $? 
 
