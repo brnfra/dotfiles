a41409e5f8b3c47eeae1d890eac89049c3beec52
brnfra <devlabbr@gmail.com>
2023-11-02 18:26:42 -0300

    add: help menu to scripts, change names filesByType and filesByDate to type_fold and date_fold and add battery (dis)charge alerts in mod-battery(i3blocks). add test if sync home and repo branch are sync

--- tests/testGitConfig.sh
+++ tests/testGitConfig.sh
@@ -22,5 +22,16 @@ testInstallHomeBranchCheck() {
 	"${BranchTest}" \
 	"${result}"
     }
+testSyncHomeRepoBranchCheck() {
+    cd $HOME
+    BranchTest="$(cat $HOME/bin/dotfiles_env | awk /branch=/'{print $1}' | sed 's/branch="//g;s/"//g')"
+    BranchHome="$(git --git-dir="$install_dir" --work-tree="$HOME" describe --all --exact-match HEAD | cut -d "/" -f 2 )";
+    cd "$dotfiles_dir"
+    RepoBranch="$(git describe --all --exact-match HEAD | cut -d "/" -f 2 )";
+    assertEquals \
+	"${LINENO}: The result of ${BranchTest}(Home dotfiles) was wrong" \
+	"${BranchHome}" \
+	"${RepoBranch}"
+    }
 
 . shunit2
