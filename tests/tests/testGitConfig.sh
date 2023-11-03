4bfa1f16eaf4cd4f2b743bac2c6aa4fcf6cc10e9
brnfra <devlabbr@gmail.com>
2023-11-03 17:44:52 -0300

    refactor: update dotfiles dir or scripts

--- tests/tests/testGitConfig.sh
+++ tests/tests/testGitConfig.sh
@@ -5,14 +5,16 @@
 SHUNIT_TEST_PREFIX=' Git Test --> '
 SHUNIT_COLOR="always"
 testInstallRepoBranchCheck() {
+    cd $dotfiles_dir
     BranchTest="$(cat $dotfiles_dir/bin/dotfiles_env | awk /branch=/'{print $1}' | sed 's/branch="//g;s/"//g')"
-    result="$(git --git-dir=$dotfiles_dir/.git describe --all --exact-match HEAD | cut -d "/" -f 2)";
+    result="$(git describe --all --exact-match HEAD | cut -d "/" -f 2 )";
     assertEquals \
 	"${LINENO}: The result of ${BranchTest} was wrong" \
 	"${BranchTest}" \
 	"${result}"
     }
 testInstallHomeBranchCheck() {
+    cd $HOME
     BranchTest="$(cat $HOME/bin/dotfiles_env | awk /branch=/'{print $1}' | sed 's/branch="//g;s/"//g')"
     result="$(git --git-dir="$install_dir" --work-tree="$HOME" describe --all --exact-match HEAD | cut -d "/" -f 2 )";
     assertEquals \
@@ -21,10 +23,13 @@ testInstallHomeBranchCheck() {
 	"${result}"
     }
 testSyncHomeRepoBranchCheck() {
+    cd $HOME
+    BranchTest="$(cat $HOME/bin/dotfiles_env | awk /branch=/'{print $1}' | sed 's/branch="//g;s/"//g')"
     BranchHome="$(git --git-dir="$install_dir" --work-tree="$HOME" describe --all --exact-match HEAD | cut -d "/" -f 2 )";
-    RepoBranch="$(git --git-dir=$dotfiles_dir/.git describe --all --exact-match HEAD | cut -d "/" -f 2)";
+    cd "$dotfiles_dir"
+    RepoBranch="$(git describe --all --exact-match HEAD | cut -d "/" -f 2 )";
     assertEquals \
-	"${LINENO}: The result of ${BranchHome}(Home dotfiles) wasn't expected(the same)" \
+	"${LINENO}: The result of ${BranchTest}(Home dotfiles) was wrong" \
 	"${BranchHome}" \
 	"${RepoBranch}"
     }
