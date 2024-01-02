78f52ca8d958446f577b6260338e0bfd1ef7e2ad
brnfra <devlabbr@gmail.com>
2024-01-02 18:22:44 -0300

    feat: add test to install script

--- tests/testInstall.sh
+++ tests/testInstall.sh
@@ -5,22 +5,44 @@
 SHUNIT_TEST_PREFIX=' Install Script Test --> '
 SHUNIT_COLOR="always"
 
-testInstallParamsRepoCheck() {
-    BranchGetFromTest="$(cat $dotfiles_dir/bin/install | sed '/curl/!d;s|https://raw.githubusercontent.com/brnfra/dotfiles/||g;s|/bin/dotfiles_env||g' | cut -d "\"" -f 2)"
-    result="$(git --git-dir=$dotfiles_dir/.git describe --all --exact-match HEAD | cut -d "/" -f 2)";
+testInstallParamsHomeCheck() {
+    
+    BranchGetFromTest="$(cat $HOME/bin/install | sed '/curl/!d;s|https://raw.githubusercontent.com/brnfra/dotfiles/||g;s|/bin/dotfiles_env||g' | cut -d "\"" -f 2)"
+    result="$(git --git-dir="$install_dir" --work-tree="$HOME" describe --all --exact-match HEAD | cut -d "/" -f 2 )";
+    clnInfo "Check install script bare Home branch $result"
     assertEquals \
 	"${LINENO}: The result of ${BranchGetFromTest} was wrong" \
 	"${BranchGetFromTest}" \
 	"${result}"
     }
 
-testInstallParamsHomeCheck() {
-    BranchGetFromTest="$(cat $HOME/bin/install | sed '/curl/!d;s|https://raw.githubusercontent.com/brnfra/dotfiles/||g;s|/bin/dotfiles_env||g' | cut -d "\"" -f 2)"
+testInstallSiteCheckHome() {
+    site="$(cat $HOME/bin/install | sed '/curl/!d' | cut -d "\"" -f 2)"
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $site)
+    result="200"
+    assertEquals \
+	"${LINENO}: The result of ${response} was wrong" \
+	"${response}" \
+	"${result}"
+    }
 
-    result="$(git --git-dir="$install_dir" --work-tree="$HOME" describe --all --exact-match HEAD | cut -d "/" -f 2 )";
+testInstallParamsRepoCheck() {
+    BranchGetFromTest="$(cat $dotfiles_dir/bin/install | sed '/curl/!d;s|https://raw.githubusercontent.com/brnfra/dotfiles/||g;s|/bin/dotfiles_env||g' | cut -d "\"" -f 2)"
+    result="$(git --git-dir=$dotfiles_dir/.git describe --all --exact-match HEAD | cut -d "/" -f 2)";
+    clnInfo "Check install script cloned repository branch $result"
     assertEquals \
 	"${LINENO}: The result of ${BranchGetFromTest} was wrong" \
 	"${BranchGetFromTest}" \
+	"${result}"
+    }
+
+testInstallSiteCheckRepo() {
+    site="$(cat $dotfiles_dir/bin/install | sed '/curl/!d' | cut -d "\"" -f 2)"
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $site)
+    result="200"
+    assertEquals \
+	"${LINENO}: The result of ${response} was wrong" \
+	"${response}" \
 	"${result}"
     }
 
