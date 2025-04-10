40310240f57795e8e24a232baf611bb1665ad936
brnfra <devlabbr@gmail.com>
2025-04-10 00:24:48 -0300

    fix merge:init stow

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 set runtimepath^=~/.vim/runtimepath runtimepath+=~/.vim/runtimepath/after
 let &packpath = &runtimepath
 source ~/.vimrc
 let g:python3_host_prog = '/usr/bin/python3'
-||||||| empty tree
-=======
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
->>>>>>> 73aac843cbd85b5133698836c50b29330cbd7794
--- test/dotfilesTest.sh
+++ test/dotfilesTest.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 # shellcheck source=~/bin/dotfiles_env
 source $HOME/bin/dotfiles_env
@@ -21,29 +20,4 @@ parallel -P 2 -j 20 bats ::: \
     testGitConfig.bats \
     testInstall.bats
 #testPkgInstalled.bats \
-||||||| empty tree
-=======
-#!/usr/bin/env bash
-# shellcheck source=~/bin/dotfiles_env
-source $HOME/bin/dotfiles_env
 
-DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
-PATH="$DIR/../src:$PATH"
-local=$(pwd)
-if [ "$local" = "$HOME" ];then
-    exit 1
-fi
-
-
-parallel bats -F pretty ::: \
-    testRepoConfigFiles.bats \
-    testHomeConfigFiles.bats \
-    testBareI3Config.bats \
-    testRepoI3Config.bats \
-    binMakeFolderByDateTest.bats \
-    binMakeFolderByTypeTest.bats \
-    binNamestdScriptTest.bats \
-    testGitConfig.bats \
-    testInstall.bats
-#testPkgInstalled.bats \
->>>>>>> 73aac843cbd85b5133698836c50b29330cbd7794
