04a584b612921876c7e2d6d1c46a33220cb35309
brnfra <devlabbr@gmail.com>
2025-04-11 22:42:02 -0300

    update dotfile test script

--- test/dotfilesTest.sh
+++ test/dotfilesTest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck source=~/bin/dotfiles_env
-source $HOME/bin/dotfiles_env
+. $HOME/bin/dotfiles_env
+
+set -e
 
 DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
 PATH="$DIR/../src:$PATH"
@@ -9,13 +11,34 @@ if [ "$local" = "$HOME" ];then
     exit 1
 fi
 
-parallel -P 2 -j 20 bats ::: \
-    testIfExistConfigFiles.bats || exit 1 \
-    testRepoI3Config.bats || exit 1\
-    binMakeFolderByDateTest.bats || exit 1\
-    binMakeFolderByTypeTest.bats || exit 1\
-    binNamestdScriptTest.bats || exit 1\
-    testGitConfig.bats || exit 1\
-    testInstall.bats || exit 1
+doit() {
+ if ! [ -e $1 ];then
+    cInfo "File not found"
+    return 1	
+ elif ! bats --print-output-on-failure  $1 ;then
+    return 2
+ else
+     clean
+ fi
+}
+export -f doit
+parallel -j 20 --bar doit ::: \
+    testIfExistConfigFiles.bats  \
+    testRepoI3Config.bats \
+    binMakeFolderByDateTest.bats \
+    binMakeFolderByTypeTest.bats \
+    binNamestdScriptTest.bats \
+    testGitConfig.bats \
+    testGitConfig2.bats \
+    testInstall.bats 
+
+#parallel -P 2 -j 20 bats ::: \
+#    testIfExistConfigFiles.bats  || exit 1 \
+#    testRepoI3Config.bats || exit 1\
+#    binMakeFolderByDateTest.bats || exit 1\
+#    binMakeFolderByTypeTest.bats || exit 1\
+#    binNamestdScriptTest.bats || exit 1\
+#    testGitConfig.bats || exit 1\
+#    testInstall.bats || exit 1
 #testPkgInstalled.bats \
 
